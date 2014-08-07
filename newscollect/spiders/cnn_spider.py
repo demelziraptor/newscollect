74c719ba3d4102fb5d12aa1105b28c16d85f6052
Demelziraptor <demelza@circularvale.com>
2014-08-07 19:29:58 +0100

    fixing cnn spider for video posts

--- newscollect/spiders/cnn_spider.py
+++ newscollect/spiders/cnn_spider.py
@@ -7,6 +7,7 @@ class CNNSpider(CrawlSpider):
     allowed_domains = ["edition.cnn.com"]
     start_urls = ["http://edition.cnn.com/WORLD/all.html"]
     user_agent = 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.99 Mobile Safari/537.36'
+    #download_delay = 100
     
     rules = (
         Rule(LinkExtractor(restrict_xpaths=('//div[@class="module_bgroup"]')), callback='parse_item'),
@@ -14,6 +15,9 @@ class CNNSpider(CrawlSpider):
     
     def parse_item(self, response):
         item = NewscollectItem()
-        item['title'] = response.xpath('//li[@class="article"]/span[@class="tline"]/text()').extract()[0]
-        item['date'] = response.xpath('//li[@class="article"]/span[@class="artday"]/text()').extract()[0]
+        try:
+            item['title'] = response.xpath('//li[@class="varticle"]/span[@class="tline"]/text()').extract()[0]
+        except:
+            item['title'] = response.xpath('//li[@class="article"]/span[@class="tline"]/text()').extract()[0]
+            item['date'] = response.xpath('//li[@class="article"]/span[@class="artday"]/text()').extract()[0]
         yield item
